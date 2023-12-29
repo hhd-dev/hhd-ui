23c8c9f4eab66bad506e6ebe135187f086424ddf
Aarron Lee <aclee91@gmail.com>
2023-12-29 17:08:08 -0500

    simple electron app with static files

---
+++ electron/README.md
@@ -0,0 +1,17 @@
+# Build Electron app
+
+```bash
+# git clone repo
+
+cd hhd-ui
+npm install
+
+npm run electron-build
+
+cd ./electron
+npm install
+
+npm run build
+
+# output will be in the electron/dist/HHD.AppImage
+```
---
+++ electron/main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow, screen: electronScreen } = require("electron");
+const path = require("path");
+
+const createMainWindow = () => {
+  let mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    show: false,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: false,
+      webSecurity: false,
+    },
+  });
+  const startURL = `file://${path.join(
+    __dirname,
+    "./static/build/index.html"
+  )}`;
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.once("ready-to-show", () => mainWindow.show());
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+};
+
+app.whenReady().then(() => {
+  createMainWindow();
+
+  app.on("activate", () => {
+    if (!BrowserWindow.getAllWindows().length) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
