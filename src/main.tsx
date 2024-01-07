c1b6dafe24b697c491ec5af658d108de83ad31f2
Aarron Lee <lee.aarron.c@gmail.com>
2024-01-07 08:17:58 -0500

    add chakra-ui

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,19 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux-modules/store.tsx";
 import TokenPrompt from "./components/TokenPrompt.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
-        <Routes>
-          <Route path="/token" Component={TokenPrompt} />
-          <Route path="/" Component={App} />
-        </Routes>
-      </HashRouter>
+      <ChakraProvider>
+        <HashRouter>
+          <Routes>
+            <Route path="/token" Component={TokenPrompt} />
+            <Route path="/" Component={App} />
+          </Routes>
+        </HashRouter>
+      </ChakraProvider>
     </Provider>
   </React.StrictMode>
 );
