4c933cd06f6bd2a7ed7e086935a484cd46536b77
antheas <git@antheas.dev>
2024-02-21 19:59:20 +0100

    fix polling

--- src/redux-modules/polling.tsx
+++ src/redux-modules/polling.tsx
@@ -2,28 +2,31 @@ import { fetchFn } from "./hhdAsyncThunks";
 import hhdSlice, { selectVersionHashes } from "./hhdSlice";
 import { store } from "./store";
 
+import { get } from "lodash";
+
 let intervalId: number | undefined;
 
 export const hhdPollingInterval = () => {
   if (!intervalId) {
     intervalId = window.setInterval(async () => {
       const state = store.getState();
+      const currHash = get(
+        state,
+        "hhd.settings.hhd.version.value",
+        get(state, "hhd.settingsState.version", "")
+      );
 
-      const settings = await fetchFn("settings");
-      const settingsState = await fetchFn("state");
+      const newState = await fetchFn("state");
+      const newHash = get(newState, "version", "");
 
-      const currentVersionHashes = selectVersionHashes(state);
-
-      const updatedVersionHashes = selectVersionHashes({
-        hhd: { settings, settingsState } as any,
-      });
-
-      if (updatedVersionHashes.settings !== currentVersionHashes.settings) {
+      // If settings changed reload them
+      if (newHash !== currHash) {
+        const settings = await fetchFn("settings");
         store.dispatch(hhdSlice.actions.overrideSettings(settings));
-        store.dispatch(hhdSlice.actions.overrideSettingsState(settingsState));
       }
-      //   if (updatedVersionHashes.state !== currentVersionHashes.state) {
-      //   }
+
+      // Apply new state
+      store.dispatch(hhdSlice.actions.overrideSettingsState(newState));
     }, 1000);
   }
 };
