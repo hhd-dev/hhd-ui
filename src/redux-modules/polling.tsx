6f6f01ac17b4cdd8d0ab24983dd1d51d9a0189ac
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-02-21 14:45:06 -0500

    add try catch to polling

--- src/redux-modules/polling.tsx
+++ src/redux-modules/polling.tsx
@@ -1,5 +1,5 @@
 import { fetchFn } from "./hhdAsyncThunks";
-import hhdSlice, { selectVersionHashes } from "./hhdSlice";
+import hhdSlice from "./hhdSlice";
 import { store } from "./store";
 
 import { get } from "lodash";
@@ -7,30 +7,34 @@ import { get } from "lodash";
 let intervalId: number | undefined;
 
 export const hhdPollingInterval = () => {
-  if (!intervalId) {
-    intervalId = window.setInterval(async () => {
-      const state = store.getState();
-      const currHash = get(
-        state,
-        "hhd.settings.hhd.version.value",
-        get(state, "hhd.settingsState.version", "")
-      );
+  try {
+    if (!intervalId) {
+      intervalId = window.setInterval(async () => {
+        const state = store.getState();
+        const currHash = get(
+          state,
+          "hhd.settings.hhd.version.value",
+          get(state, "hhd.settingsState.version", "")
+        );
 
-      const newState = await fetchFn("state");
-      const newHash = get(newState, "version", "");
+        const newState = await fetchFn("state");
+        const newHash = get(newState, "version", "");
 
-      // If settings changed reload them
-      if (newHash !== currHash) {
-        const settings = await fetchFn("settings");
-        store.dispatch(hhdSlice.actions.overrideSettings(settings));
-      }
+        // If settings changed reload them
+        if (newHash !== currHash) {
+          const settings = await fetchFn("settings");
+          store.dispatch(hhdSlice.actions.overrideSettings(settings));
+        }
 
-      if (
-        JSON.stringify(state.hhd.settingsState) !== JSON.stringify(newState)
-      ) {
-        // Apply new state
-        store.dispatch(hhdSlice.actions.overrideSettingsState(newState));
-      }
-    }, 1000);
+        if (
+          JSON.stringify(state.hhd.settingsState) !== JSON.stringify(newState)
+        ) {
+          // Apply new state
+          store.dispatch(hhdSlice.actions.overrideSettingsState(newState));
+        }
+      }, 1000);
+    }
+  } catch (e) {
+    console.log(e);
   }
 };
