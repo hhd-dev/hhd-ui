09e824c5fc5052ac18238dfd7c9c48f6e9897a11
antheas <git@antheas.dev>
2024-02-27 20:33:25 +0100

    add hanging reads for polling

--- src/redux-modules/polling.tsx
+++ src/redux-modules/polling.tsx
@@ -4,43 +4,74 @@ import { store } from "./store";
 
 import { get } from "lodash";
 
-let intervalId: number | undefined;
+let abort: AbortController | undefined;
+
+const MIN_WAITTIME_NEW = 250;
+const MIN_WAITTIME_OLD = 1000;
+
+async function pollState(a: AbortController) {
+  try {
+    while (!a.signal.aborted) {
+      const start = Date.now();
+
+      // Fetch
+      const state = store.getState();
+      const currHash = get(
+        state,
+        "hhd.settings.hhd.version.value",
+        get(state, "hhd.settingsState.version", "")
+      );
+
+      const newState = await fetchFn("state", { signal: a.signal });
+      const newHash = get(newState, "version", "");
+
+      // If settings changed reload them
+      if (newHash !== currHash) {
+        const settings = await fetchFn("settings", { signal: a.signal });
+        store.dispatch(hhdSlice.actions.overrideSettings(settings));
+      }
+
+      // Apply new state
+      if (
+        JSON.stringify(state.hhd.settingsState) !== JSON.stringify(newState)
+      ) {
+        store.dispatch(hhdSlice.actions.overrideSettingsState(newState));
+      }
+
+      const del = Date.now() - start;
+      let waitTime: number | null = null;
+      if (del < 100) {
+        // If less than 100ms, we are using a legacy
+        // handheld daemon version. Avoid overloading.
+        waitTime = MIN_WAITTIME_OLD - del;
+      } else if (del < MIN_WAITTIME_NEW) {
+        waitTime = MIN_WAITTIME_NEW - del;
+      }
+
+      if (waitTime != null) {
+        const actWaitTime = waitTime;
+        const p = new Promise((r) =>
+          setTimeout(r, actWaitTime, { signal: a.signal })
+        );
+        await p;
+      }
+    }
+  } catch (e) {}
+}
 
 export const hhdPollingInterval = () => {
   try {
-    if (!intervalId) {
-      intervalId = window.setInterval(async () => {
-        const state = store.getState();
-        const currHash = get(
-          state,
-          "hhd.settings.hhd.version.value",
-          get(state, "hhd.settingsState.version", "")
-        );
+    if (abort) return;
 
-        const newState = await fetchFn("state");
-        const newHash = get(newState, "version", "");
-
-        // If settings changed reload them
-        if (newHash !== currHash) {
-          const settings = await fetchFn("settings");
-          store.dispatch(hhdSlice.actions.overrideSettings(settings));
-        }
-
-        if (
-          JSON.stringify(state.hhd.settingsState) !== JSON.stringify(newState)
-        ) {
-          // Apply new state
-          store.dispatch(hhdSlice.actions.overrideSettingsState(newState));
-        }
-      }, 1000);
-    }
+    abort = new AbortController();
+    pollState(abort);
   } catch (e) {
     console.log(e);
   }
   return () => {
-    if (intervalId) {
-      clearInterval(intervalId);
-      intervalId = undefined;
+    if (abort) {
+      abort.abort();
+      abort = undefined;
     }
   };
 };
