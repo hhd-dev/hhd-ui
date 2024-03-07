87215a3033471840781fa3efc01f864beab24ab7
antheas <git@antheas.dev>
2024-03-07 23:47:44 +0100

    ignore guide combos

--- src/model/controller.tsx
+++ src/model/controller.tsx
@@ -12,6 +12,7 @@ const BUTTON_MAP = {
   b: 1,
   x: 2,
   y: 3,
+  guide: 16,
 };
 
 const REPEAT_INITIAL = 500;
@@ -92,6 +93,9 @@ export const setupGamepadEventListener = () => {
 
       // Handle gamepad events
       if (!evs) continue;
+
+      // Ignore guide combos
+      if (state[gidx].guide) continue;
 
       const uiType = selectUiType(store.getState());
       const appType = selectAppType(store.getState());
