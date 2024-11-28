323178645378a6d3351f077d972c480d83001f0d
Antheas Kapenekakis <git@antheas.dev>
2024-11-28 17:44:49 +0100

    allow mode to hide its structure

--- src/components/elements/ModeComponent.tsx
+++ src/components/elements/ModeComponent.tsx
@@ -28,13 +28,14 @@ import { getButtonStyle, getFocusStyle } from "./utils";
 
 const ModeComponent: FC<ModeProps> = ({ settings: set, path, section }) => {
   const { state, setState } = useSettingState<string>(`${path}.mode`);
-  const { title, modes, hint } = set;
+  const { title, modes, hint, tags } = set;
   const shouldRenderChild = useShouldRenderChild(section === "qam");
   const { ref, focus, setFocus } = useElementNav(section, path);
   const { colorMode } = useColorMode();
   const isDisabled = useDisabledTooltip();
   // const dispatch = useDispatch();
 
+  const drop = !tags?.includes("no_dropdown");
   const mode = state ? set.modes[state] : null;
 
   const { state: colorState } = useSettingState<
@@ -50,82 +51,88 @@ const ModeComponent: FC<ModeProps> = ({ settings: set, path, section }) => {
   >(path || "");
   return (
     <>
-      <Box {...getFocusStyle(focus, colorMode)}>
-        <Tooltip label={hint} isDisabled={isDisabled}>
-          <FormLabel htmlFor={path}>{title}</FormLabel>
-        </Tooltip>
-        <Menu>
-          <Tooltip label={mode?.hint} isDisabled={isDisabled}>
-            <MenuButton
-              as={Button}
-              width="100%"
-              ref={ref}
-              onFocus={setFocus}
-              rightIcon={<ChevronDownIcon />}
-              marginBottom="0.3rem"
-              // onClick={() => dispatch(dispatch(slice.actions.select()))}
-              {...getButtonStyle(
-                state ? (set as ModeSetting).modes[state]?.tags : undefined,
-                colorState && state ? colorState[state] : undefined
-              )}
-            >
-              {`${mode?.title}${mode?.unit ? ` (${mode.unit})` : ""}`}
-            </MenuButton>
+      {drop && (
+        <Box {...getFocusStyle(focus, colorMode)}>
+          <Tooltip label={hint} isDisabled={isDisabled}>
+            <FormLabel htmlFor={path}>{title}</FormLabel>
           </Tooltip>
-          <MenuList zIndex={100}>
-            <MenuOptionGroup type="radio" value={state}>
-              {Object.entries(modes).map(
-                ([value, { title: label, unit }], idx: number) => {
-                  const btnStyle = getButtonStyle(
-                    value ? (set as ModeSetting).modes[value]?.tags : undefined,
-                    colorState ? colorState[value] : undefined
-                  );
-                  let extraStyles = {};
+          <Menu>
+            <Tooltip label={mode?.hint} isDisabled={isDisabled}>
+              <MenuButton
+                as={Button}
+                width="100%"
+                ref={ref}
+                onFocus={setFocus}
+                rightIcon={<ChevronDownIcon />}
+                marginBottom="0.3rem"
+                // onClick={() => dispatch(dispatch(slice.actions.select()))}
+                {...getButtonStyle(
+                  state ? (set as ModeSetting).modes[state]?.tags : undefined,
+                  colorState && state ? colorState[state] : undefined
+                )}
+              >
+                {`${mode?.title}${mode?.unit ? ` (${mode.unit})` : ""}`}
+              </MenuButton>
+            </Tooltip>
+            <MenuList zIndex={100}>
+              <MenuOptionGroup type="radio" value={state}>
+                {Object.entries(modes).map(
+                  ([value, { title: label, unit }], idx: number) => {
+                    const btnStyle = getButtonStyle(
+                      value
+                        ? (set as ModeSetting).modes[value]?.tags
+                        : undefined,
+                      colorState ? colorState[value] : undefined
+                    );
+                    let extraStyles = {};
 
-                  if (Object.keys(btnStyle).length > 0)
-                    extraStyles = {
-                      // textAlign: "center",
-                      paddingLeft: "3.3rem",
-                      borderRadius: "8px",
-                      width: "calc(100% - 0.45rem - 4px)",
-                      margin: "0.45rem 0.25rem 0 0.35rem",
-                      marginBottom: "0",
-                    };
+                    if (Object.keys(btnStyle).length > 0)
+                      extraStyles = {
+                        // textAlign: "center",
+                        paddingLeft: "3.3rem",
+                        borderRadius: "8px",
+                        width: "calc(100% - 0.45rem - 4px)",
+                        margin: "0.45rem 0.25rem 0 0.35rem",
+                        marginBottom: "0",
+                      };
 
-                  if (idx == 0) {
-                    extraStyles = {
-                      ...extraStyles,
-                      marginTop: "0rem",
-                    };
+                    if (idx == 0) {
+                      extraStyles = {
+                        ...extraStyles,
+                        marginTop: "0rem",
+                      };
+                    }
+
+                    let suffix = unit ? ` (${unit})` : "";
+
+                    return (
+                      <MenuItemOption
+                        key={idx}
+                        value={value}
+                        onClick={() => setState(value)}
+                        {...btnStyle}
+                        {...extraStyles}
+                      >
+                        {`${label}${suffix}`}
+                      </MenuItemOption>
+                    );
                   }
-
-                  let suffix = unit ? ` (${unit})` : "";
-
-                  return (
-                    <MenuItemOption
-                      key={idx}
-                      value={value}
-                      onClick={() => setState(value)}
-                      {...btnStyle}
-                      {...extraStyles}
-                    >
-                      {`${label}${suffix}`}
-                    </MenuItemOption>
-                  );
-                }
-              )}
-            </MenuOptionGroup>
-          </MenuList>
-        </Menu>
-      </Box>
-      <Flex direction="row" margin="0.2rem 0.1rem 0.4rem 0.3rem">
-        <Center>
-          <Divider
-            orientation="vertical"
-            marginRight="0.75rem"
-            alignSelf="stretch"
-          ></Divider>
-        </Center>
+                )}
+              </MenuOptionGroup>
+            </MenuList>
+          </Menu>
+        </Box>
+      )}
+      <Flex direction="row" margin={drop ? "0.2rem 0.1rem 0.4rem 0.3rem" : ""}>
+        {drop && (
+          <Center>
+            <Divider
+              orientation="vertical"
+              marginRight="0.75rem"
+              alignSelf="stretch"
+            ></Divider>
+          </Center>
+        )}
         <Flex direction="column" flexGrow="1">
           {mode &&
             shouldRenderChild(mode) &&
